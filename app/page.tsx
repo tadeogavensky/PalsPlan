0294879969e00ffec53cdcbf5361a99aefa7fdaf
tadeogavensky <tadeogavensky.email@gmail.com>
2024-05-17 18:28:04 -0300

    new db

--- app/page.tsx
+++ app/page.tsx
@@ -21,18 +21,27 @@ const getPlans = async () => {
   const apiUrl = process.env.API_URL;
 
   if (!apiUrl) {
-    console.error('API_URL is not defined');
+    console.error("API_URL is not defined");
     return [];
   }
 
   console.log("apiUrl PLANS:>> ", apiUrl);
 
   try {
-    const response = await axios.get(`https://pals-plan-fhxwywlot-tadeogavensky1s-projects.vercel.app/api/plans`);
+    const response = await axios.get(
+      `https://pals-plan-fhxwywlot-tadeogavensky1s-projects.vercel.app/api/plans`,
+      {
+        headers: {
+          Authorization:
+            "Bearer " +
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnenBlY29ybmlhbHNucmh0Y3h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU5NzU5OTgsImV4cCI6MjAzMTU1MTk5OH0.ZAxHkMB81oq5SLNchFKT-Rl26j_ca60YMjHYIJHnpx4",
+        },
+      }
+    );
     console.log("response.data :>> ", response.data);
     return response.data;
   } catch (error) {
-    console.error('Error fetching plans', error);
+    console.error("Error fetching plans", error);
     return [];
   }
 };
@@ -41,21 +50,29 @@ const getCategories = async () => {
   const apiUrl = process.env.API_URL;
 
   if (!apiUrl) {
-    console.error('API_URL is not defined');
+    console.error("API_URL is not defined");
     return [];
   }
 
   console.log("apiUrl CATEGORIES :>> ", apiUrl);
 
   try {
-    const response = await axios.get(`https://pals-plan-fhxwywlot-tadeogavensky1s-projects.vercel.app/api/categories`);
+    const response = await axios.get(
+      `https://pals-plan-fhxwywlot-tadeogavensky1s-projects.vercel.app/api/categories`,
+      {
+        headers: {
+          Authorization:
+            "Bearer " +
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnenBlY29ybmlhbHNucmh0Y3h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU5NzU5OTgsImV4cCI6MjAzMTU1MTk5OH0.ZAxHkMB81oq5SLNchFKT-Rl26j_ca60YMjHYIJHnpx4",
+        },
+      }
+    );
     console.log("response.data :>> ", response.data);
     return response.data;
   } catch (error) {
-    console.error('Error fetching categories', error);
+    console.error("Error fetching categories", error);
     return [];
   }
 };
-
 
 export default Home;
