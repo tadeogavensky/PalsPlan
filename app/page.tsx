9be30d29e7c0eec7942f34dd0164f3663f1a939b
tadeogavensky <tadeogavensky.email@gmail.com>
2024-05-17 17:37:16 -0300

    new db

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
+"use client";
+
 import Header from "@/components/Header";
 import Plans from "@/components/Plans";
 import axios from "axios";
 
 const Home = async () => {
   const plans = await getPlans();
-  const categories = await getCategories();
+/*   const categories = await getCategories(); */
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-white font-roboto m-10">
       <Header />
-      <Plans plans={plans} categories={categories} />
+    {/*   <Plans plans={plans} categories={categories} /> */}
     </main>
   );
 };
@@ -26,20 +28,9 @@ const getPlans = async () => {
   console.log("apiUrl PLANS:>> ", apiUrl);
 
   try {
-    const apiUrl = "https://pals-plan.vercel.app"; // Asegúrate de que apiUrl esté definido
-
-    const response = await fetch(`${apiUrl}/api/plans`);
-
-    if (!response.ok) {
-      // Maneja el error si la respuesta no es exitosa
-      console.error("Error al obtener las categorías:", response.statusText);
-      return;
-    }
-
-    const data = await response.json(); // Lee el cuerpo de la respuesta como JSON
-    console.log("response.data :>> ", data);
-
-    return data;
+    const response = await axios.get(`${apiUrl}/api/plans`);
+    console.log("response.data :>> ", response.data);
+    return response.data;
   } catch (error) {
     console.error(`Error fetching plans`, error);
   }
@@ -56,20 +47,9 @@ const getCategories = async () => {
   console.log("apiUrl CATEGORIES :>> ", apiUrl);
 
   try {
-    const apiUrl = "https://pals-plan.vercel.app"; // Asegúrate de que apiUrl esté definido
-
-    const response = await fetch(`${apiUrl}/api/categories`);
-
-    if (!response.ok) {
-      // Maneja el error si la respuesta no es exitosa
-      console.error("Error al obtener las categorías:", response.statusText);
-      return;
-    }
-
-    const data = await response.json(); // Lee el cuerpo de la respuesta como JSON
-    console.log("response.data :>> ", data);
-
-    return data;
+    const response = await axios.get(`${apiUrl}/api/categories`);
+    console.log("response.data :>> ", response.data);
+    return response.data;
   } catch (error) {
     console.error(`Error fetching categories`, error);
   }
