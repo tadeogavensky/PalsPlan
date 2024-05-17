b8085e4dc4ec598855bc6ffead05c61217e5c8d5
tadeogavensky <tadeogavensky.email@gmail.com>
2024-05-17 17:11:22 -0300

    new db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,12 @@
-import { PrismaClient } from '@prisma/client'
+// lib/prisma.js
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
